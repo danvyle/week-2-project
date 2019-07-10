6daa4ee435336c36f2713caef1db52c51f2877c1
github username <github email address>
2019-07-09 20:57:06 -0700

    auth files

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,24 @@
+class AuthController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:auth][:username])
+    if user && user.authenticate(params[:auth][:password])
+      session[:user_id] = user.id
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+
+  private
+  def session_params
+    params.require(:auth).permit(:username, :password)
+  end
+end
